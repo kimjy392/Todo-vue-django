ea7d20d18e28cf904d02a56919f8d59e043f9947
김준영 <kimjy392@gmail.com>
2019-11-18 14:07:56 +0900

    Django | Add Todo Index view

--- todo-django/todos/views.py
+++ todo-django/todos/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Todo
+from .serializers import TodoSerializers
 # Create your views here.
+
+@api_view(['GET'])
+def todo_index_create(index):
+    todos = Todo.objects.all()
+    serializer = TodoSerializers(todos, many=True)
+    return Response(serializer.data)
