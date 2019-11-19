78e6fbf79478b7121eed17d38ebdf70b6e895336
김준영 <kimjy392@gmail.com>
2019-11-19 12:05:06 +0900

    Vue | Login Component + vue-session

--- todo-vue/src/main.js
+++ todo-vue/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueSession from 'vue-session'
 
 Vue.config.productionTip = false
+// vue-session 서맃
+// VueSession 추가
+Vue.use(VueSession)
 
 new Vue({
   router,
---
+++ todo-vue/src/views/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="login">
+      <h2>로그인</h2>
+      <LoginForm/>
+  </div>
+</template>
+
+<script>
+import LoginForm from '@/components/LoginForm.vue'
+export default {
+  name: 'Login',
+  components: {
+    LoginForm
+  },
+  methods: {
+    // login(credentials) {
+    //   axios.post('http://127.0.0.1:8000/api-token-auth/', credentials)
+    //     .then(response => {
+    //       console.log(response)
+    //       console.log(response.data.token)
+    //       const token = response.data.token
+    //       this.$session.start()
+    //       this.$session.set('jwt', token)
+    //     })
+    //     .catch(error => {
+    //       console.log(error)
+    //     })
+    // }
+  }
+}
+</script>
+
+<style>
+
+</style>
