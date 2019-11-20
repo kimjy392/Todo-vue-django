37c6de68db253c6cc87f241d9b205dfb87a9090c
김준영 <kimjy392@gmail.com>
2019-11-20 10:36:29 +0900

    Vue | Vuex - token

--- todo-vue/src/main.js
+++ todo-vue/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueSession from 'vue-session'
+import store from './store' // vuex
 
 Vue.config.productionTip = false
 // vue-session 서맃
@@ -10,5 +11,6 @@ Vue.use(VueSession)
 
 new Vue({
   router,
+  store, // vuex
   render: h => h(App)
 }).$mount('#app')
